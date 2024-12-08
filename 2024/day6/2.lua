20cab7b1910ace8c1c0d400491d0c772484ca338
Matthew <matthewelento29@gmail.com>
2024-12-08 19:14:40 +0800

    refactor

--- 2024/day6/2.lua
+++ 2024/day6/2.lua
@@ -31,19 +31,28 @@ end
 -- Find the direction and location of the guard
 function check_line(line)
 	local directions = { '^', '>', 'v', '<' }
-	local norm = { ['^'] = { 0, 1 }, ['>'] = { 1, 0 }, ['v'] = { 0, -1 }, ['<'] = { -1, 0 } }
-	local ninetys = { ['^'] = norm['>'], ['>'] = norm['v'], ['v'] = '<', ['<'] = '^' }
+	local move = { ['^'] = { 0, -1 }, ['>'] = { 1, 0 }, ['v'] = { 0, 1 }, ['<'] = { -1, 0 } }
+	local ninetys = { ['^'] = '>', ['>'] = 'v', ['v'] = '<', ['<'] = '^' }
 	for i, v in ipairs(line) do
 		for _, s in ipairs(directions) do
 			if v == s then
-				return i,
-					{
-						symbol = s,
-						x = norm[s][1],
-						y = norm[s][2],
-						x90 = ninetys[s][1],
-						y90 = ninetys[s][2],
-					}
+				local guard = {
+					symbol = s,
+					symbol90 = ninetys[s],
+					x = move[s][1],
+					y = move[s][2],
+					x90 = move[ninetys[s]][1],
+					y90 = move[ninetys[s]][2],
+				}
+				function guard.rotate()
+					guard.symbol = guard.symbol90
+					guard.symbol90 = ninetys[guard.symbol90]
+					guard.x = guard.x90
+					guard.y = guard.y90
+					guard.x90 = move[ninetys[guard.symbol]][1]
+					guard.y90 = move[ninetys[guard.symbol]][2]
+				end
+				return i, guard
 			end
 		end
 	end
@@ -63,9 +72,6 @@ function check_guard(map)
 	return nil, nil, nil
 end
 
-print(check_guard(input))
-print_map(input)
-
 local total = 0
 local x, y, sym = check_guard(input)
 if x == nil or y == nil or sym == nil then
@@ -75,88 +81,29 @@ end
 
 local alts = { ['^'] = 'u', ['>'] = 'r', ['v'] = 'd', ['<'] = 'l' }
 while true do
-	print(input[y][x])
+	input[y][x] = 'X'
 
-	print(sym.x)
-	print(input[y + sym.y][x + sym.x])
-
-	local limity = 0 < (y + sym.y) and (y + sym.y) < #input
-	local limitx = 0 < (x + sym.x) and (x + sym.x) < #input[1]
+	local limity = 0 < (y + sym.y) and (y + sym.y) < #input + 1
+	local limitx = 0 < (x + sym.x) and (x + sym.x) < #input[1] + 1
 	if not (limity and limitx) then
 		break
 	end
 
-	-- printnorm[direction][0]
-
-	-- if direction == '%^' then
-	-- 	if y == 1 then
-	-- 		break
-	-- 	end
-	--
-	-- 	local future = string.sub(input[y - 1], x, x)
-	-- 	if future == '#' then
-	-- 		if x == #input then
-	-- 			break
-	-- 		end
-	-- 		input[y] = ReplaceChar(input[y], x + 1, '>')
-	-- 	else
-	-- 		if future == 'r' then
-	-- 			total = total + 1
-	-- 		end
-	-- 		input[y - 1] = ReplaceChar(input[y - 1], x, '^')
-	-- 	end
-	-- elseif direction == '>' then
-	-- 	if x == #input then
-	-- 		break
-	-- 	end
-	--
-	-- 	local future = string.sub(input[y], x + 1, x + 1)
-	-- 	if future == '#' then
-	-- 		if y == #input then
-	-- 			break
-	-- 		end
-	-- 		input[y + 1] = ReplaceChar(input[y + 1], x, 'v')
-	-- 	else
-	-- 		if future == 'd' then
-	-- 			total = total + 1
-	-- 		end
-	-- 		input[y] = ReplaceChar(input[y], x + 1, '>')
-	-- 	end
-	-- elseif direction == 'v' then
-	-- 	if y == #input then
-	-- 		break
-	-- 	end
-	--
-	-- 	local future = string.sub(input[y + 1], x, x)
-	-- 	if future == '#' then
-	-- 		if x == 1 then
-	-- 			break
-	-- 		end
-	-- 		input[y] = ReplaceChar(input[y], x - 1, '<')
-	-- 	else
-	-- 		if future == 'l' then
-	-- 			total = total + 1
-	-- 		end
-	-- 		input[y + 1] = ReplaceChar(input[y + 1], x, 'v')
-	-- 	end
-	-- elseif direction == '<' then
-	-- 	if x == 1 then
-	-- 		break
-	-- 	end
-	--
-	-- 	local future = string.sub(input[y], x - 1, x - 1)
-	-- 	if future == '#' then
-	-- 		if y == 1 then
-	-- 			break
-	-- 		end
-	-- 		input[y - 1] = ReplaceChar(input[y - 1], x, '^')
-	-- 	else
-	-- 		if future == 'u' then
-	-- 			total = total + 1
-	-- 		end
-	-- 		input[y] = ReplaceChar(input[y], x - 1, '<')
-	-- 	end
-	-- end
+	local future = input[y + sym.y][x + sym.x]
+	if future == '#' then
+		local limity90 = 0 < (y + sym.y90) and (y + sym.y90) < #input + 1
+		local limitx90 = 0 < (x + sym.x90) and (x + sym.x90) < #input[1] + 1
+		if not (limity90 and limitx90) then
+			break
+		end
+		input[y + sym.y90][x + sym.x90] = sym.symbol90
+		sym.rotate()
+	else
+		input[y + sym.y][x + sym.x] = sym.symbol
+	end
+	y = y + sym.y
+	x = x + sym.x
 end
 
+print_map(input)
 print(total)
