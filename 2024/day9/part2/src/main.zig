8345ab7b389db1a1a2ca57222b80285ae4c1e2b2
Matthew <matthewelento29@gmail.com>
2024-12-25 10:25:26 +0800

    gappin

--- 2024/day9/part2/src/main.zig
+++ 2024/day9/part2/src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
-const test_allocator = std.testing.allocator;
+const disk_map = struct { pos: usize, size: usize };
+const printf = std.log.info;
 const gap = 48;
 
 pub fn main() !void {
@@ -13,69 +14,56 @@ pub fn main() !void {
     const file_size = try file.getEndPos();
     const allocator = std.heap.page_allocator;
     var input = try file.readToEndAlloc(allocator, file_size);
-    input = input[0 .. input.len - 1];
+    input = input[0 .. input.len - 1]; // Remove the null bit
     defer allocator.free(input);
 
-    const block_length: usize = get_block_length(input);
-    const block = try allocator.alloc(usize, block_length);
+    const block = try allocator.alloc(usize, get_block_length(input));
     defer allocator.free(block);
-
     make_block(block, input);
 
-    // std.log.info("{any}", .{block});
+    printf("{s}", .{input});
     print_block(block);
-    defragment(block, input);
-    print_block(block);
-    // std.log.info("{any}", .{block});
 
-    std.log.info("{}", .{checksum(block)});
+    const full = try make_disk_map(allocator, input, 0);
+    const blank = try make_disk_map(allocator, input, 1);
+    defer allocator.free(full);
+    defer allocator.free(blank);
+    // defragment(block, full, blank);
+
+    printf("full {any}", .{full});
+    printf("blank {any}", .{blank});
+    print_block(block);
+
+    printf("{}", .{checksum(block)});
 }
 
-fn print_block(block: []usize) void {
-    for (block) |char| {
-        if (char == std.math.maxInt(usize)) {
-            std.debug.print("{c}", .{'.'});
-        } else {
-            std.debug.print("{}", .{char});
-        }
-    }
-    std.debug.print("\n", .{});
-}
-
-fn defragment(block: []usize, input: []u8) void {
-    var i: usize = 1;
-    var j: usize = input.len - 1;
-    while (i < input.len - 1) : (i += 2) {
-        while (j > 0) : (j -= 1) {
-            if (!is_even(j)) {
-                continue;
-            }
-
-            const i_size: u8 = input[i] - gap;
-            const j_size: u8 = input[j] - gap;
-
-            if (j_size == 0) {
-                continue;
-            }
-            if (i_size >= j_size) {
-                const ind_i = get_index(input, i);
-                const ind_j = get_index(input, j);
-                std.log.info("i: {} ind_i: {} i_size: {} j: {} ind_j: {} j_size: {}", .{ i, ind_i, i_size, j, ind_j, j_size });
-
-                var k: u8 = 0;
-                while (k < j_size) : (k += 1) {
-                    block[ind_i + k] = block[ind_j + k];
-                    block[ind_j + k] = std.math.maxInt(usize);
-                }
-                input[j] = gap;
-                input[i] -= j_size;
-                input[i - 1] += j_size;
-                break;
-            }
-        }
-    }
-
+fn defragment(block: []usize, full: []disk_map, blank: []disk_map) void {
+    _ = block;
+    _ = full;
+    _ = blank;
     return;
+}
+
+fn make_disk_map(allocator: std.mem.Allocator, input: []u8, offset: usize) ![]disk_map {
+    const block = try allocator.alloc(disk_map, (input.len + 1) / 2);
+
+    var shit: usize = offset;
+    var i: usize = offset;
+    while (i < input.len) : (i += 2) {
+        shit += input[i] - gap;
+        const pos = ((i + 1) / 2);
+        block[pos].pos = shit;
+        block[pos].size = input[i] - gap;
+    }
+    return block;
+}
+
+fn get_block_length(input: []u8) usize {
+    var block_length: usize = 0;
+    for (input) |char| {
+        block_length += char - gap;
+    }
+    return block_length;
 }
 
 fn make_block(block: []usize, input: []u8) void {
@@ -92,14 +80,6 @@ fn make_block(block: []usize, input: []u8) void {
             }
         }
     }
-}
-fn get_index(input: []u8, n: usize) usize {
-    var index: usize = 0;
-    var i: usize = 0;
-    while (i < n) : (i += 1) {
-        index += input[i] - gap;
-    }
-    return index;
 }
 
 fn checksum(block: []const usize) u64 {
@@ -120,13 +100,15 @@ fn is_even(x: usize) bool {
     return false;
 }
 
-fn get_block_length(input: []u8) usize {
-    var block_length: usize = 0;
-    for (input) |char| {
-        // std.log.info("{} - {}", .{ char, gap });
-        block_length += char - gap;
+fn print_block(block: []usize) void {
+    for (block) |char| {
+        if (char == std.math.maxInt(usize)) {
+            std.debug.print("{c}", .{'.'});
+        } else {
+            std.debug.print("{}", .{char});
+        }
     }
-    return block_length;
+    std.debug.print("\n", .{});
 }
 
 test "checksum" {
@@ -137,58 +119,17 @@ test "checksum" {
 }
 
 test "odd" {
-    const zero: usize = 0 / 2;
-    const odd1: usize = 1 / 2;
-    const even1: usize = 2 / 2;
-    const odd2: usize = 3 / 2;
-    const even2: usize = 4 / 2;
-
-    try std.testing.expectEqual(0, zero);
-    try std.testing.expectEqual(0, odd1);
-    try std.testing.expectEqual(1, even1);
-    try std.testing.expectEqual(1, odd2);
-    try std.testing.expectEqual(2, even2);
+    try std.testing.expectEqual(0, 0 / 2);
+    try std.testing.expectEqual(0, 1 / 2);
+    try std.testing.expectEqual(1, 2 / 2);
+    try std.testing.expectEqual(1, 3 / 2);
+    try std.testing.expectEqual(2, 4 / 2);
 }
 
-test "get_index" {
-    const input_length = 6;
-    const input = try test_allocator.alloc(u8, input_length);
-    defer test_allocator.free(input);
-    input[0] = '1';
-    input[1] = '2';
-    input[2] = '3';
-    input[3] = '4';
-    input[4] = '5';
-    input[5] = '6';
-
-    const block = try test_allocator.alloc(usize, get_block_length(input));
-    defer test_allocator.free(block);
-
-    make_block(block, input);
-    // print_block(block);
-    try std.testing.expectEqual(0, get_index(input, 0));
-    try std.testing.expectEqual(1, get_index(input, 1));
-    try std.testing.expectEqual(3, get_index(input, 2));
-    try std.testing.expectEqual(6, get_index(input, 3));
-    try std.testing.expectEqual(10, get_index(input, 4));
-    try std.testing.expectEqual(15, get_index(input, 5));
-}
-
-test "defragment" {
-    const input = try test_allocator.alloc(u8, 4);
-    defer test_allocator.free(input);
-    input[0] = '1';
-    input[1] = '2';
-    input[2] = '3';
-    input[3] = '4';
-
-    var block_length: usize = 0;
-    for (input) |char| {
-        // std.log.info("{} - {}", .{ char, gap });
-        block_length += char - gap;
-    }
-    const block = try test_allocator.alloc(usize, block_length);
-    defer test_allocator.free(block);
-
-    // print_block(block);
+test "ceil" {
+    try std.testing.expectEqual(0, (0 + 1) / 2);
+    try std.testing.expectEqual(1, (1 + 1) / 2);
+    try std.testing.expectEqual(1, (2 + 1) / 2);
+    try std.testing.expectEqual(2, (3 + 1) / 2);
+    try std.testing.expectEqual(2, (4 + 1) / 2);
 }
