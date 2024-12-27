d4e8e87c64b83fc3e6746d844707f5d98d74ce35
Matthew <matthewelento29@gmail.com>
2024-12-27 08:46:43 +0800

    YYOOO

--- 2024/day11/1-loop.sh
+++ 2024/day11/1-loop.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-str=$(cat $1)
+FILE=$1
 
-readarray -d " " -t arr <<<$str
+read -a arr <"$FILE"
 
 i=0
 max=$2
@@ -11,34 +11,39 @@ size=${#arr[@]}
 echo ${arr[@]}
 
 while [ $i -lt $max ]; do
-        echo "Current: $((i + 1))"
+        echo "Current: $((i + 1)) Stones: $size"
         j=0
         while [ $j -lt $size ]; do
-                arr[j]=$((10#${arr[j]}))
-
                 rock_len=${#arr[j]}
-                if [ ${arr[j]} = 0 ]; then
+                if ((arr[j] == 0)); then
                         arr[j]="1"
                 elif ((rock_len % 2 == 0)); then
-                        # echo "before: ${arr[@]}"
+                        ## loop
                         rock_len_half=$((rock_len / 2))
                         arr+=(${arr[-1]})
                         for ((k = size; k > j; k--)); do
                                 arr[k]=${arr[((k - 1))]}
                         done
                         arr[j]=${arr[j]:0:rock_len_half}
-                        arr[j + 1]=${arr[j + 1]:rock_len_half:rock_len}
+                        val=${arr[j + 1]:rock_len_half}
+                        val=${val#0}
+                        arr[j + 1]=${val-0}
 
+                        ## matching and shi
+                        # ((rock_len_half = rock_len / 2))
+                        # middle_right=${arr[j]:rock_len_half}
+                        # middle_right=${middle_right#0}
+                        # middle_right=${middle_right:-0}
+                        # arr=(${arr[@]:0:j} ${arr[j]:0:rock_len_half} $middle_right ${arr[@]:((j + 1)):size})
                         ((size++))
                         ((j++))
-                        # echo "after: ${arr[@]}"
                 else
-                        arr[j]=$((arr[j] * 2024))
+                        ((arr[j] *= 2024))
                 fi
 
                 ((j++))
         done
-        # echo "$i: ${arr[@]}"
+        # echo "$((i + 1)): ${arr[@]}"
         ((i++))
 done
 
