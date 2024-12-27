e69f90d4b61834ba974ee6af90c0a43c4335cf94
Matthew <matthewelento29@gmail.com>
2024-12-27 09:37:10 +0800

    fucking hell man

--- 2024/day11/1.sh
+++ 2024/day11/1.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-str=$(cat $1)
-
-readarray -d " " -t arr <<<$str
+read -a arr <$1
 
 i=0
 max=$2
@@ -11,27 +9,25 @@ size=${#arr[@]}
 echo ${arr[@]}
 
 while [ $i -lt $max ]; do
-        echo "Current: $((i + 1))"
+        echo "Current: $((i + 1)) Stones: $size"
         j=0
         while [ $j -lt $size ]; do
                 arr[j]=$((10#${arr[j]}))
+
+                rock_len=${#arr[j]}
                 if [ ${arr[j]} = 0 ]; then
                         arr[j]="1"
-                elif ((${#arr[j]} % 2 == 0)); then
+                elif ((rock_len % 2 == 0)); then
                         # echo "before: ${arr[@]}"
-                        # left=${arr[@]:0:$j}
-                        # right=${arr[@]:((j + 1)):size}
-                        # middle_left=${arr[j]:0:((${#arr[j]} / 2))}
-                        # middle_right=${arr[j]:((${#arr[j]} / 2)):${#arr[j]}}
-                        # arr=($left $middle_left $middle_right $right)
-                        # echo "left: ${left[@]}"
-                        # echo "middle_left: $middle_left"
-                        # echo "middle_right: $middle_right"
-                        # echo "right: ${right[@]}"
-
-                        # inline version of this
-
-                        arr=(${arr[@]:0:$j} ${arr[j]:0:((${#arr[j]} / 2))} ${arr[j]:((${#arr[j]} / 2)):${#arr[j]}} ${arr[@]:((j + 1)):size})
+                        rock_len_half=$((rock_len / 2))
+                        arr+=(${arr[-1]})
+                        k=$size
+                        while [[ $k -gt $j ]]; do
+                                arr[k]=${arr[((k - 1))]}
+                                ((k--))
+                        done
+                        arr[j]=${arr[j]:0:rock_len_half}
+                        arr[j + 1]=${arr[j + 1]:rock_len_half:rock_len}
 
                         ((size++))
                         ((j++))
@@ -47,5 +43,4 @@ while [ $i -lt $max ]; do
 done
 
 echo "----"
-# echo ${arr[@]}
 echo "len: $size"
---
+++ 2024/day11/2.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+read -a arr <$1
+
+i=0
+max=$2
+echo ${arr[@]}
+
+apply() {
+        local -a new_arr
+        for stone in "${arr[@]}"; do
+                stone=$((10#${stone}))
+                stone_len=${#stone}
+                if ((10#$stone == 0)); then
+                        new_arr+=(1)
+                elif ((stone_len % 2 == 0)); then
+                        ((stone_len_half = stone_len / 2))
+                        new_arr+=(${stone:0:stone_len_half})
+                        new_arr+=(${stone:stone_len_half})
+                else
+                        ((stone *= 2024))
+                        new_arr+=($stone)
+                fi
+        done
+        arr=("${new_arr[@]}")
+}
+
+for ((i = 0; i < max; i++)); do
+        echo "Current: $((i + 1)) Stones: ${#arr[@]}"
+        apply
+done
+
+echo "----"
+echo "len: ${#arr[@]}"
