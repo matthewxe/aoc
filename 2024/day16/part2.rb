038bb1956d4a910d52cab16c2d0653b1d891885d
Matthew <matthewelento29@gmail.com>
2025-02-02 21:54:37 +0800

    rubi

--- 2024/day16/part2.rb
+++ 2024/day16/part2.rb
@@ -14,37 +14,38 @@ end
 
 Coordinates = Struct.new(:x, :y)
 
-class Reindeer
-  @@visited = Set.new
-  @@direction = Hash.new
-  @@queue = Queue.new
-  @@score = Hash.new
-  def initialize(coords, map)
-    @@visited << coords
-    @@queue << coords
-    @@direction[coords] = Coordinates.new(1, 0)
-    @@score[coords] = 0
-    @final_score = nil
-    @map = map
-  end
+map = file.lines.map do |line|
+  line.split("")
+end
 
-  def simulate
-    while not @@queue.empty?
-      self.step
+start_coords = nil
+end_coords = nil
+
+map.each_with_index do |line, y|
+  line.each_with_index do |block, x|
+    if block == "S"
+      start_coords = Coordinates.new(x, y)
+    elsif block == "E"
+      end_coords = Coordinates.new(x, y)
     end
   end
+end
 
-  def step
-    current = @@queue.pop
-    # puts(current)
-    # puts("visited", @@visited)
-    # puts("queue", @@queue)
-    add_unvisited(current)
-    return current
-  end
+Path = Struct.new(:score, :visited)
+Vector = Struct.new(:position, :direction)
 
-  def add_unvisited(current)
-    direction = @@direction[current]
+def simulate(map, exit, current, direction, visited, score)
+  possible = []
+  lowest_score = nil
+  total_visited = Set.new
+  visited_score = Hash.new
+
+  stack = []
+  stack.push(current, direction, visited, score)
+
+  while not stack.empty?
+    current, direction, visited, score = stack.pop(4)
+
     directions = [
       [direction.x, direction.y, 1],
       [direction.y * -1, direction.x, 1001],
@@ -52,48 +53,44 @@ class Reindeer
     ]
 
     directions.each do |d|
-      space = get_relative_space(current, d[0], d[1])
-      ds = Coordinates.new(d[0], d[1])
-      coords = Coordinates.new(current.x + ds.x, current.y + ds.y)
-      if space != "#" and
-          (not @@visited.include?(coords) or
-          @@score[current] +
-          d[2] < @@score[coords])
-        @@visited << coords
-        @@queue << coords
-        @@direction[coords] = ds
-        @@score[coords] = @@score[current] + d[2]
-      end
+      new_direction = Coordinates.new(d[0], d[1])
+      space = map[current.y + new_direction.y][current.x + new_direction.x]
+      coords = Coordinates.new(current.x + new_direction.x, current.y + new_direction.y)
 
       if space == "E"
-        new_final_score = @@score[current] + d[2]
-        if @final_score == nil or
-            new_final_score < @final_score
-          @final_score = new_final_score
+        if lowest_score == nil or
+            score +
+            d[2] <= lowest_score
+          possible += [Path.new(score + d[2], visited)]
+          lowest_score = score + d[2]
+          puts(lowest_score)
         end
+      elsif space != "#" and
+          (lowest_score == nil or
+            score +
+            d[2] <= lowest_score) and
+          not visited.include?(coords) and
+          (not total_visited.include?(Vector.new(coords, new_direction)) or
+            visited_score[Vector.new(coords, new_direction)] >= score +
+              d[2])
+        stack.push(coords, new_direction, visited | [coords], score + d[2])
+        vector = Vector.new(coords, new_direction)
+        total_visited << vector
+        visited_score[vector] = score + d[2]
       end
     end
   end
 
-  def get_relative_space(current, x, y)
-    return @map[current.y + y][current.x + x]
-  end
-
-  def final_score
-    return @final_score
-  end
+  return possible
 end
 
-map = file.lines.map do |line|
-  line.split("")
-end
+possible = simulate(map, end_coords, start_coords, Coordinates.new(1, 0), Set[start_coords, end_coords], 0)
 
-map.each_with_index do |line, y|
-  line.each_with_index do |block, x|
-    if block == "S"
-      honse = Reindeer.new(Coordinates.new(x, y), map)
-      honse.simulate
-      puts(honse.final_score)
-    end
-  end
-end
+min = possible.min { |a, b| a.score <=> b.score }.score
+final = Set.new
+possible = possible
+  .select { |path| min == path.score }
+  .map { |path| final += path.visited }
+
+puts(min)
+puts(final.length)
